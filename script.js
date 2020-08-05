f9a07748baf5ab7ac33f06c743c29b8434ead70e
peacefrog <gregroyclark@gmail.com>
2020-08-05 18:40:50 -0500

    adds getRandomLower function

---
+++ script.js
@@ -0,0 +1,7 @@
+// Generator functions - http://www.net-comber.com/charset.html
+
+getRandomLower = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+console.log(getRandomLower());
