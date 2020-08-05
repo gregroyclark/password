96c5910a71a57b1811a00d84c4167c1990e3d9d6
peacefrog <gregroyclark@gmail.com>
2020-08-05 18:42:08 -0500

    adds getRandomNumber function

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ getRandomUpper = () => {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 };
 
+getRandomNumber = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 48);
+};
